99f6c6c53d36ad07a2fe1adceefd14ce449a42a1
Diegosny <diegojamesmetalica@gmail.com>
2021-06-08 20:18:30 -0300

    Importando Jquery

--- resources/views/eventos/cadastro.blade.php
+++ resources/views/eventos/cadastro.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.app')
 
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header"> Cadastro De Eventos </div>
-  
 
                 <div class="card-body">
                       <form action="{{ route('cadastro.pessoa') }}" method="POST">
@@ -72,26 +70,7 @@
                           Launch demo modal
                         </button>
 
-                        <!-- Modal -->
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                          <div class="modal-dialog">
-                            <div class="modal-content">
-                              <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                              </div>
-                              <div class="modal-body">
-                                ...
-                              </div>
-                              <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-
-
+                  
                         </div>
                     </form>
                 </div>
@@ -100,6 +79,7 @@
     </div>
 </div>
 
+<script src="{{ asset('js/eventos/cadastro.js') }}"></script> 
+
 @endsection
 
-<script src="{{ asset('js/eventos/cadastro.js') }}"></script> 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/scripts/jquery-3.6.0.min.js') }}"></script> 
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <script src="{{ asset('js/sweetalert2/dist/sweetalert2.min.js') }}"></script>  
 
     <!-- Fonts -->
